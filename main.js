f5ef2ff44857fc20e34c000e972b0f19e1e23337
nicklassienczak <nicklas-sk@hotmail.com>
2022-10-09 19:41:32 +0200

    BMI Calculator

---
+++ main.js
@@ -0,0 +1,43 @@
+const btn = document.getElementById("calcButton");
+
+btn.addEventListener("click", function(){
+
+    let height = document.querySelector("#height").value;
+    let weight = document.querySelector("#weight").value;
+//.value is to indicate to the following if-sentence, that we want to use the input value,
+// and not the query/label itself
+    if (height === "" || weight === "") {
+        alert("Please fill in your height and weight.")
+        return;
+    }
+
+    height = (height / 100)
+
+    let BMI = (weight / (height*height))
+    BMI = BMI.toFixed(2)
+    //Log to test
+    console.log(BMI)
+
+    //innerHTML to get the result into the site
+    document.getElementById("bmiResult").innerHTML = BMI;
+
+
+    let bmiStatus = "";
+
+    if (BMI < 18.5) {
+        bmiStatus = "underweight";
+    }
+    // && tells the code, that both sides of the statement must be true.
+    if (BMI >= 18.5 && BMI < 25) {
+        bmiStatus = "normal weight";
+    }
+    if (BMI >= 25 && BMI < 30) {
+        bmiStatus = "overweight";
+    }
+    if (BMI > 30) {
+        bmiStatus = "obese";
+    }
+
+    document.querySelector("#comment").innerHTML = `Some people would say that you're ${bmiStatus}.`
+
+});
